cbc6567562d5755a26f4bf79e534e955315fdb50
rikgj <44588337+rikgj@users.noreply.github.com>
2020-11-21 22:48:30 +0100

    read testwords from file

---
+++ password-lookup/passwordlookup.py
@@ -0,0 +1,53 @@
+import urllib.request, urllib.error, urllib.parse
+import hashlib
+
+testlist = []
+sha1list = []
+hitlist = []
+
+f = open('testlist.csv',"r")
+lines = f.readlines()
+for line in lines:
+    line = line.rstrip()
+    line = line.split(",")
+    for l in line:
+        testlist.append(l)
+
+
+for test in testlist:
+    m = hashlib.sha1()
+    test = test.encode('utf-8')
+    m.update(test)
+    sha1list.append(m.hexdigest().upper())
+
+
+baseURL = 'https://api.pwnedpasswords.com/range/'
+count = 0
+
+for hash in sha1list:
+    url = baseURL + hash[0:5]
+    response = urllib.request.urlopen(url)
+    webContent = response.read()
+    content = webContent.decode('ASCII')
+
+    f = open('dump.txt',"w")
+    f.write(content)
+    f.close()
+
+    f = open('dump.txt',"r")
+    lines = f.readlines()
+    found = False
+    i = 0
+    limit = len(lines)
+    while found == False and i < limit:
+        line = lines[i]
+        pair = line.split(":")
+
+        if(pair[0] == hash[5:]):
+            hit = [testlist[count], int(pair[1])]
+            hitlist.append(hit)
+            found = True
+        i+=1
+    count+=1
+
+print(hitlist)
